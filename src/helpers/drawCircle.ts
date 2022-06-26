4e07dc8c5f752507c5463543505ed773e2afbcaf
ShlemenKirill <kirill.shlemen@gmail.com>
2022-06-26 12:33:54 +0400

    Update drawCircle.ts

--- src/helpers/drawCircle.ts
+++ src/helpers/drawCircle.ts
@@ -2,11 +2,13 @@ import robot from "robotjs";
 
 const drawCircle = (radius: number) => {
     const mousePos = robot.getMousePos();
+    robot.mouseToggle('down');
     for (let i = 0; i <= Math.PI * 2; i += 0.01) {
         const x = mousePos.x + radius * Math.cos(i);
         const y = mousePos.y + radius * Math.sin(i);
         robot.dragMouse(x, y);
     }
+    robot.mouseToggle('up');
 };
 
 export default drawCircle
