47902201a159fb2be051e9bdbfd37698850c91d9
ShlemenKirill <kirill.shlemen@gmail.com>
2022-06-26 12:33:45 +0400

    Update printScreen.ts

--- src/helpers/printScreen.ts
+++ src/helpers/printScreen.ts
@@ -1,31 +1,16 @@
-import Jimp from 'jimp'
-import { screen } from 'robotjs'
+import Jimp from 'jimp';
+import { screen } from 'robotjs';
 
-export default async function printScreen(
-    x: number,
-    y: number,
-    width: number,
-    height: number
-): Promise<Jimp> {
-    return new Promise((resolve) => {
-        const bitmap = screen.capture(x, y, width, height)
-        const image = new Jimp(bitmap.width, bitmap.height)
+const printScreen = (x: number, y: number, width: number, height: number) => {
+	const screenCapture = screen.capture(x, y, width, height);
+	const jimpImage = new Jimp(screenCapture.width, screenCapture.height);
+	let position = 0;
+	return jimpImage.scan(0, 0, jimpImage.bitmap.width, jimpImage.bitmap.height, (x, y, idx) => {
+		jimpImage.bitmap.data[idx + 2] = screenCapture.image.readUInt8(position++);
+		jimpImage.bitmap.data[idx + 1] = screenCapture.image.readUInt8(position++);
+		jimpImage.bitmap.data[idx + 0] = screenCapture.image.readUInt8(position++);
+		jimpImage.bitmap.data[idx + 3] = screenCapture.image.readUInt8(position++);
+	});
+};
 
-        let pos = 0
-
-        image.scanQuiet(
-            0,
-            0,
-            image.bitmap.width,
-            image.bitmap.height,
-            (x, y, idx) => {
-                image.bitmap.data[idx + 2] = bitmap.image.readUInt8(pos++)
-                image.bitmap.data[idx + 1] = bitmap.image.readUInt8(pos++)
-                image.bitmap.data[idx + 0] = bitmap.image.readUInt8(pos++)
-                image.bitmap.data[idx + 3] = bitmap.image.readUInt8(pos++)
-            }
-        )
-
-        resolve(image)
-    })
-}
+export default printScreen;
